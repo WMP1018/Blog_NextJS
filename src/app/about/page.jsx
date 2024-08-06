0ae72e311797e365aa4416fdafb8821f0dd056a9
Wandy Muñoz <whandy_1018@hotmail.com>
2024-08-06 18:57:42 -0500

    Clase-3

---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function about(){
+    return(
+        <div>
+            <h1>About</h1>
+            <p1>Esta es la pagina sobre</p1>
+        </div>
+    )
+}
---
+++ src/app/blog/page.jsx
@@ -0,0 +1,7 @@
+export default function post(){
+    return(
+        <div>
+            <h1>Blog de pruebas</h1>
+        </div>
+    )
+}
