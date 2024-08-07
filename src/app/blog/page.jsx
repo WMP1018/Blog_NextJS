5fca28ff4d0d0f85a63846afe73b1814a61f13e6
Wandy Muñoz <whandy_1018@hotmail.com>
2024-08-06 21:08:22 -0500

    Clase-5

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,10 +4,37 @@ export const metadata = {
     keywords: ['p1','p2']
 }
 
-export default function Post(){
+export default async function Post(){
+    const data = await getData();
     return(
         <div>
             <h1>Blog de pruebas</h1>
+            <p>Este es el contenido del blog</p>
+            <br></br>
+            <br></br>
+            <ul>
+                {data.map(  ({id, title, body}) => (
+                    <li key={id}>
+                        <h3>{id} -- {title}</h3>
+                        <p>{body}</p>
+                        <br></br>
+                    </li>
+                ))                
+                }
+            </ul>
         </div>
     )
+}
+
+async function getData() {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if(!res.ok){
+            throw new Error('Hubo un error en la red.');
+        }
+        const posts = await res.json();
+        return posts;
+    } catch (error) {
+        console.error(error);
+    }
 }
